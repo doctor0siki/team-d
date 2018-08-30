fcbd69c0569560f2a7c00e4679f8e6fbec988b78
ceres\y-fukumoto <y-fukumoto@ceres-inc.jp>
2018-08-30 16:49:15 +0900

    update setting

--- src/settings.php
+++ src/settings.php
@@ -31,11 +31,11 @@ return [
 
             'connection' => [
                 'driver' => 'pdo_mysql',
-                'host' => 'localhost',
+                'host' => 'intern-db',
                 'port' => '3306',
-                'user' => 'team-a',
-                'password' => 'team-a!',
-                'dbname' => 'teama',
+                'user' => 'team-d',
+                'password' => 'team-d!',
+                'dbname' => 'teamd',
                 'charset' => 'utf8'
             ]
         ]
