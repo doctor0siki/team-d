3ef9e679f00b1cf9127d0b2ad0281f827875b767
rabbi-natu <n.imai.asagi@gmail.com>
2019-08-29 17:39:11 +0900

    材料と作り方のDB追加とフロント調整

---
+++ app/Controller/recipe/detail.php
@@ -0,0 +1,26 @@
+<?php
+use Model\Dao\Recipe;
+use Slim\Http\Request;
+use Slim\Http\Response;
+/**
+ * 商品一覧を出すコントローラです
+ *
+ * detail/1 = 1番の商品を
+ * detail/13 = 13番の商品を
+ * 表示する仕組みになっています。
+ *
+ * {item_id}の中身は$argsに入ります。
+ * 取得する時は、$args["item_id"]で取得できます。
+ */
+$app->get('/recipe/detail/{recipe_id}', function (Request $request, Response $response, $args) {
+    $data = [];
+    //URLパラメータのitem_idを取得します。
+    $recipe_id = $args["recipe_id"];
+    // var_dump($recipe_id);die;
+    //アイテムDAOをインスタンス化します。
+    $recipe = new Recipe($this->db);
+    //URLパラメータのitem_id部分を引数として渡し、戻り値をresultに格納します
+    $data["result"] = $recipe->getRecipeList($recipe_id);
+    // Render index view
+    return $this->view->render($response, 'recipe/detail.twig', $data);
+});
