882fe4e58e3fa91c111731a4d16f4f6a5fbb05d8
rabbi-natu <n.imai.asagi@gmail.com>
2019-09-05 23:39:19 +0900

    エラー修正

--- app/Controller/refrigerator/index.php
+++ app/Controller/refrigerator/index.php
@@ -12,18 +12,18 @@ $app->get('/refrigerator/index', function (Request $request, Response $response)
     return $this->view->render($response, 'refrigerator/index.twig', $data);
 });
 
-$app->get('/refrigerator/add', function (Request $request, Response $response) {
-
-    $data = [];
-
-    // Render index view
-    return $this->view->render($response, 'refrigerator/add.twig', $data);
-});
-
-$app->get('/refrigerator/list', function (Request $request, Response $response) {
-
-    $data = [];
-
-    // Render index view
-    return $this->view->render($response, 'refrigerator/list.twig', $data);
-});
+// $app->get('/refrigerator/add', function (Request $request, Response $response) {
+//
+//     $data = [];
+//
+//     // Render index view
+//     return $this->view->render($response, 'refrigerator/add.twig', $data);
+// });
+//
+// $app->get('/refrigerator/list', function (Request $request, Response $response) {
+//
+//     $data = [];
+//
+//     // Render index view
+//     return $this->view->render($response, 'refrigerator/list.twig', $data);
+// });
